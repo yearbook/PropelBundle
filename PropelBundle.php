66b74c6299db1d933999afc2cd654fc9d3d5fdac
Peter Potrowl <peter.potrowl@gmail.com>
2022-03-14 11:49:45 +0100

    Fix compatibility with Propel master.
    We had:
    
    Compile Error: Declaration of Propel\Bundle\PropelBundle\DependencyInjection\Configuration::addPathsSection(Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $node) must be compatible with Propel\Common\Config\PropelConfiguration::addPathsSection(Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $node): void
    
    Compile Error: Declaration of Propel\Bundle\PropelBundle\DependencyInjection\Configuration::addDatabaseSection(Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $node) must be compatible with Propel\Common\Config\PropelConfiguration::addDatabaseSection(Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition $node): void
    
    Symfony\Component\Debug\Exception\FatalThrowableError^ {#33
      -originalClassName: "ArgumentCountError"
      #message: "Too few arguments to function Propel\Runtime\Connection\ConnectionManagerSingle::__construct(), 0 passed in /var/www/html/vendor/skyfox/propel-bundle/PropelBundle.php on line 81 and exactly 1 expected"
      #code: 0
      #file: "./vendor/propel/propel/src/Propel/Runtime/Connection/ConnectionManagerSingle.php"

--- PropelBundle.php
+++ PropelBundle.php
@@ -78,12 +78,12 @@ class PropelBundle extends Bundle
 
                 $manager->setReadConfiguration($slaveConnections);
             } else {
-                $manager = new ConnectionManagerSingle();
+                $manager = new ConnectionManagerSingle($name);
                 $manager->setConfiguration($connection);
             }
 
             $serviceContainer->setAdapterClass($name, $connection['adapter']);
-            $serviceContainer->setConnectionManager($name, $manager);
+            $serviceContainer->setConnectionManager($manager);
 
             // load database maps
             if(file_exists($config['paths']['loaderScriptDir'].'/loadDatabase.php') && is_readable($config['paths']['loaderScriptDir'].'/loadDatabase.php')) {
